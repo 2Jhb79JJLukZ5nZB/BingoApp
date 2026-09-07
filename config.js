376f4184c9c0dd7d9a02d18f87da234fc31b3a4e
2Jhb79JJLukZ5nZB <2Jhb79JJLukZ5nZB@proton.me>
2026-09-08 04:12:16 +0900

    Update Apps Script deployment URL

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 "use strict";
 window.BINGO_CONFIG = {
-  appsScriptUrl: "https://script.google.com/macros/s/AKfycbw-DaqOSYyXpXFuXBSGFt9RYAmsT6wgM2zAPIzOIHA39Cz-g54QqF4A85zZ45BZZEoL/exec",
+  appsScriptUrl: "https://script.google.com/macros/s/AKfycbxfOwKIzBkj4uIL42V1D5FEMR2ZXDb_ZD_J0VTw0FBKPlwuaYevYDW1JSvqxNUBZOwF/exec",
   slidesUrl: "https://docs.google.com/presentation/d/e/2PACX-1vSzIxbaBBK4gC2dAX1DN__5YAatrCnROIr-iCfshRJ-JOKgQm0nsgcUKgmOs1wSFMVTMlfKOMc5VwWH/pubembed?start=false&loop=false&delayms=60000"
 };
