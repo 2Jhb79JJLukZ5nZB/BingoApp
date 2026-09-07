b5d05c32f32f2b979e3eccb7e044f68d55d2f594
2Jhb79JJLukZ5nZB <2Jhb79JJLukZ5nZB@proton.me>
2026-09-08 03:58:25 +0900

    Connect Google Sheets web app

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 "use strict";
 window.BINGO_CONFIG = {
-  appsScriptUrl: "",
+  appsScriptUrl: "https://script.google.com/a/macros/minami-h.tym.ed.jp/s/AKfycbyTo8snLk9XPyo0Tb895k9SvKGHLC25fS1RrujE2ocp1H2EBFmy11bZoyfEVS39Khc/exec",
   slidesUrl: "https://docs.google.com/presentation/d/e/2PACX-1vSzIxbaBBK4gC2dAX1DN__5YAatrCnROIr-iCfshRJ-JOKgQm0nsgcUKgmOs1wSFMVTMlfKOMc5VwWH/pubembed?start=false&loop=false&delayms=60000"
 };
