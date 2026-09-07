136afb0d664f9811b4e4c01e71c4f07a1cc8f95c
2Jhb79JJLukZ5nZB <2Jhb79JJLukZ5nZB@proton.me>
2026-09-08 04:03:16 +0900

    Use public Google Sheets web app

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 "use strict";
 window.BINGO_CONFIG = {
-  appsScriptUrl: "https://script.google.com/a/macros/minami-h.tym.ed.jp/s/AKfycbyTo8snLk9XPyo0Tb895k9SvKGHLC25fS1RrujE2ocp1H2EBFmy11bZoyfEVS39Khc/exec",
+  appsScriptUrl: "https://script.google.com/macros/s/AKfycbw-DaqOSYyXpXFuXBSGFt9RYAmsT6wgM2zAPIzOIHA39Cz-g54QqF4A85zZ45BZZEoL/exec",
   slidesUrl: "https://docs.google.com/presentation/d/e/2PACX-1vSzIxbaBBK4gC2dAX1DN__5YAatrCnROIr-iCfshRJ-JOKgQm0nsgcUKgmOs1wSFMVTMlfKOMc5VwWH/pubembed?start=false&loop=false&delayms=60000"
 };
